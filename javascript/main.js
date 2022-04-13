a5a54c17a951d8fcaeffb15b2fce40c192bb652e
damar <damarisriverosg@gmail.com>
2022-04-13 18:36:23 -0400

    todas las validaciones de registro y nuevo inicio de sesion

--- javascript/main.js
+++ javascript/main.js
@@ -6,15 +6,18 @@ function contrasenna() {
 
   if (contra1 == "", contra2 == "") {
     document.getElementById("errorcontrasena").innerHTML = "debe escribir una contraseña";
+    document.getElementById("contrasena").style.borderColor="#dc143c";
+    document.getElementById("peace").style.borderColor="#dc143c";
   } else {
 
   }
   if (contra1 != contra2) {
     document.getElementById("errorcontrasena").innerHTML = "las contraseñas no son iguales";
-
+    document.getElementById("contrasena").style.borderColor="#dc143c";
+    document.getElementById("peace").style.borderColor="#dc143c";
   }
 
-
+  
 }
 
 
@@ -22,25 +25,42 @@ function email() {
   var email = document.getElementById("emaail").value;
   var a = /^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
 
-  if (email.lenght == 0) {
+  if (email.length == 0) {
     document.getElementById("erroremail").innerHTML = "debe escribir un email";
-    document.getElementById("email").style.color="#dc143c";
+    document.getElementById("emaail").style.borderColor="#dc143c";
   } else {
 
   }
   if (!a.exec(email)) {
     document.getElementById("erroremail").innerHTML = "el email no esta bien escrito";
-    document.getElementById("email").style.color="#dc143c";
+    document.getElementById("emaail").style.borderColor="#dc143c";
   }
 
 }
 
 function nombre(){
+  var nombre = document.getElementById("nombrefr").value;
+  var apellido= document.getElementById("apellidofr").value;
+  if(nombre.length == 0 && apellido.length==0){
+    document.getElementById("errornombre").innerHTML= "debe escribir un nombre";
+    document.getElementById("errorapellido").innerHTML="debe escribir al menos 1 apellido ";
+    document.getElementById("nombrefr").style.borderColor="#dc143c";
+    document.getElementById("apellidofr").style.borderColor="#dc143c";
+  }
+}
 
+function telefono(){
+  var telefono= document.getElementById("telefono").value;
+  if(telefono.length==0){
+    document.getElementById("errortelefono").innerHTML="debe escribir un telefono";
+    document.getElementById("telefono").style.borderColor="#dc143c";
+  }
 }
 
 function todos() {
   contrasenna();
   email();
+  nombre();
+  telefono();
 }
 
