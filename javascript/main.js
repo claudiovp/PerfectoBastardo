c45a69250c5855c4e5a443f1d4cdce4971b242f9
damar <damarisriverosg@gmail.com>
2022-04-13 12:34:19 -0400

    arreglo de validacion de email

--- javascript/main.js
+++ javascript/main.js
@@ -4,25 +4,43 @@ function contrasenna() {
   var contra1 = document.getElementById("contrasena").value;
   var contra2 = document.getElementById("peace").value;
 
-  if(contra1=="",contra2==""){
-      document.getElementById("errorcontrasena").innerHTML="debe escribir una contraseña";
-  }    else {
-    
-    }if (contra1 != contra2) {
+  if (contra1 == "", contra2 == "") {
+    document.getElementById("errorcontrasena").innerHTML = "debe escribir una contraseña";
+  } else {
+
+  }
+  if (contra1 != contra2) {
     document.getElementById("errorcontrasena").innerHTML = "las contraseñas no son iguales";
 
   }
+
+
 }
 
-function email(){
-  var email = Document.getElementById("email").value;
 
-  if(email==""){
-    document.getElementById("erroremail").innerHTML= "debe escribir un email";
-  }else{
+function email() {
+  var email = document.getElementById("emaail").value;
+  var a = /^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
 
-  }if(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/.test(email)){
-    document.getElementById("erroremail").innerHTML= "el email no esta bien escrito";
+  if (email.lenght == 0) {
+    document.getElementById("erroremail").innerHTML = "debe escribir un email";
+    document.getElementById("email").style.color="#dc143c";
+  } else {
+
   }
-  
+  if (!a.exec(email)) {
+    document.getElementById("erroremail").innerHTML = "el email no esta bien escrito";
+    document.getElementById("email").style.color="#dc143c";
+  }
+
 }
+
+function nombre(){
+
+}
+
+function todos() {
+  contrasenna();
+  email();
+}
+
